304e8cb5dd818de63292ae50e4e1ade97ba18b91
kelvinlimjk <kelvinlim.jk@gmail.com>
2016-07-20 07:48:29 +0800

    Update and rename demo file.txt to demo file.js

---
+++ demo file.js
@@ -0,0 +1,16 @@
+{ "items": [
+ {
+ "id": 1,
+ "quote": "Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do.",
+ "author": "Pele",
+ },  {
+ "id": 2,
+ "quote": "Never stop fighting until you arrive at your destined place - that is, the unique you. Have an aim in life, continuously acquire knowledge, work hard, and have perseverance to realise the great life.",
+ "author": "A. P. J. Abdul Kalam",
+ },   {
+ "id": 3,
+ "quote": "test",
+ "author": "test",
+ }
+
+
